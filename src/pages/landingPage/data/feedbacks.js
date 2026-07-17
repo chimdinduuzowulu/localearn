cf55e82987cdc327bb693ae810200e82f96daa81
Chimdindu Uzowulu <chimdindu73@gmailcom>
2026-07-17 10:06:06 +0100

    Testimonial update

--- src/pages/landingPage/data/feedbacks.js
+++ src/pages/landingPage/data/feedbacks.js
@@ -5,7 +5,7 @@ export const feedbacks = [
     name: "Aisha Bello",
     title: "Teacher",
     testimony:
-      "Wootlab Foundation has done wonders with our local languages..Thank you for bringing learning down to grassroot. we really appreciate.",
+      "Wootlab Foundation has done wonders. Thank you for bringing learning down to grassroot, we really appreciate.",
   },
   {
     id: 2,
@@ -13,7 +13,7 @@ export const feedbacks = [
     name: "Olatunji Samuel",
     title: "Student",
     testimony:
-      "At first, learning to use a computer was really tough. But with daily practice, it became simple.  Thank you to WootlabLab for this opportunity!",
+      "At first, learning to use a computer was really tough. But with daily practice, it became simple.  Thank you to Wootlab for this opportunity!",
   },
   {
     id: 3,
@@ -21,6 +21,6 @@ export const feedbacks = [
     name: "Promise Samson",
     title: "Student",
     testimony:
-      "Thanks to Wootlab Now, I can analyze data and confidently use  PowerPoint. I feel 100% ready for the job market and confident in my skills. I am incredibly grateful !",
+      "Thanks to Wootlab. Now, I can analyze data and confidently use  PowerPoint. I feel 100% ready for the job market and confident in my skills. I am incredibly grateful!",
   },
 ];
